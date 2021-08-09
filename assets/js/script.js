2dfa7fe70a88cc985aacc0550be1b8ccd2ccb95f
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2021-08-09 09:31:51 +0000

    create outline js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-console.log('testing!');
+/** code for 'MOVIE QUOTES QUIZ' */
+
+function submitQuiz() {
+    
+    function answerTally () {
+
+    }
+
+/** use answerTally function to calculate score*/ 
+    var calculateScore =
+
+
+    function correctAnswer () {
+
+    }
+
+    /** if statments for correctAnswer function */
+    {
+
+    }
+
+    var questionCount =
+
+
+    var showResults =
