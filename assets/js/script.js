6776e000873ff7256820a28f50799f7f745f0c58
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2021-08-11 10:08:37 +0000

    create function to check answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,13 +18,20 @@ function submitQuiz() {
     return answerValue;
 }
 
-/** use answerTally function to calculate score*/ 
-    var calculateScore =
+    /** use answerTally function to calculate score*/ 
+    var calculateScore = (answerTally('question1') + answerTally('question2') + answerTally('question3') + answerTally('question4')
+    + answerTally('question5') + answerTally('question6') + answerTally('question7') + answerTally('question8') 
+    + answerTally('question9') + answerTally('question10'));
+    console.log("CalculateScore: " + calculateScore); 
 
-
-    function correctAnswer () {
-
+    /** use correctAnswer function to return correct option as a string */
+    function correctAnswer (correctOptionNumber, questionNumber) {
+        console.log("questionNumber: " + questionNumber);  // logs 1,2,3,4 after called below
+        return ("The correct answer for question " + questionNumber + " is &nbsp;<strong>" +
+            (document.getElementById(correctOptionNumber).innerHTML) + "</strong>");
     }
+
+    
 
     /** if statments for correctAnswer function */
     {
