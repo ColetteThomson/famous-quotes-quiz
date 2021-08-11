70bdbed379aaf4c222a8536ae1f1489ec572d53a
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2021-08-11 10:03:13 +0000

    create answerTally function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,22 @@
 /** code for 'MOVIE QUOTES QUIZ' */
 
 function submitQuiz() {
-    
-    function answerTally () {
+    console.log('submitted');
 
+    /**  get each answer tally and its radio button value */
+    function answerTally (qName) {
+        var radioButtons = document.getElementsByName(qName);
+
+        for (var i = 0, length = radioButtons.length; i < length; i++) {
+               if (radioButtons[i].checked) {
+                    var answerValue = Number(radioButtons[i].value);
+        }
     }
+    if (isNaN(answerValue)) {
+        answerValue = 0;
+    }
+    return answerValue;
+}
 
 /** use answerTally function to calculate score*/ 
     var calculateScore =
