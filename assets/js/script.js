8a143eadd4b632dce1147dc9e00afaf43e8bb54a
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2021-08-13 11:02:13 +0000

    edit correctAnswer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,41 +26,42 @@ function submitQuiz() {
 
     /** use correctAnswer function to return correct option as a string */
     function correctAnswer (correctOptionNumber, questionNumber) {
-        console.log("questionNumber: " + questionNumber);  // logs 1,2,3,4 after called below
+        console.log("questionNumber: " + questionNumber);  
+
         return ("The correct answer for question " + questionNumber + " is &nbsp;<strong>" +
             (document.getElementById(correctOptionNumber).innerHTML) + "</strong>");
     }
 
     /** use correctAnswer function to print correct answers, if user answers incorrectly */
     if (answerTally('question1') === 0) {
-        document.getElementById('correctAnswer1').innerHTML = correctAnswer('correctOption1', 1);
+        document.getElementById('userAnswer1').innerHTML = correctAnswer('correctOption1', 1);
     }
     if (answerTally('question2') === 0) {
-        document.getElementById('correctAnswer2').innerHTML = correctAnswer('correctOption2', 2);
+        document.getElementById('userAnswer2').innerHTML = correctAnswer('correctOption2', 2);
     }
     if (answerTally('question3') === 0) {
-        document.getElementById('correctAnswer3').innerHTML = correctAnswer('correctOption3', 3);
+        document.getElementById('userAnswer3').innerHTML = correctAnswer('correctOption3', 3);
     }
     if (answerTally('question4') === 0) {
-        document.getElementById('correctAnswer4').innerHTML = correctAnswer('correctOption4', 4);
+        document.getElementById('userAnswer4').innerHTML = correctAnswer('correctOption4', 4);
     }
     if (answerTally('question5') === 0) {
-        document.getElementById('correctAnswer5').innerHTML = correctAnswer('correctOption5', 5);
+        document.getElementById('userAnswer5').innerHTML = correctAnswer('correctOption5', 5);
     }
     if (answerTally('question6') === 0) {
-        document.getElementById('correctAnswer6').innerHTML = correctAnswer('correctOption6', 6);
+        document.getElementById('userAnswer6').innerHTML = correctAnswer('correctOption6', 6);
     }
     if (answerTally('question7') === 0) {
-        document.getElementById('correctAnswer7').innerHTML = correctAnswer('correctOption7', 7);
+        document.getElementById('userAnswer7').innerHTML = correctAnswer('correctOption7', 7);
     }
     if (answerTally('question8') === 0) {
-        document.getElementById('correctAnswer8').innerHTML = correctAnswer('correctOption8', 8);
+        document.getElementById('userAnswer8').innerHTML = correctAnswer('correctOption8', 8);
     }
     if (answerTally('question9') === 0) {
-        document.getElementById('correctAnswer9').innerHTML = correctAnswer('correctOption9', 9);
+        document.getElementById('userAnswer9').innerHTML = correctAnswer('correctOption9', 9);
     }
     if (answerTally('question10') === 0) {
-        document.getElementById('correctAnswer10').innerHTML = correctAnswer('correctOption10', 10);
+        document.getElementById('userAnswer10').innerHTML = correctAnswer('correctOption10', 10);
     }
 
     /** calculate number of questions using html class 'question'*/
